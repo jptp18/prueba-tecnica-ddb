fe328d7fd45e6f021cf161ecb3b7bb2e949885b2
Juan Trejo <juanpatp08@gmail.com>
2023-06-02 16:37:18 -0500

    Primer commit, subida a repositorio de todas las carpetas

---
+++ banners/300x100/index.js
@@ -0,0 +1,43 @@
+const animate = () => {
+
+    //Timeline para darle orden a las animaciones
+    const tl = gsap.timeline();
+
+    //Animaciones
+    tl.from(".banner",{ 
+        duration: 1,
+        scale: 0.5, 
+        opacity: 0, 
+        delay: 0.5, 
+        stagger: 0.2,
+        ease: "elastic", 
+    });
+
+    tl.from(".logo",{
+        duration: 1, 
+        ease: "sine.in", 
+        x: -500  
+    });
+
+    tl.from(".title",{ 
+        duration: 1, 
+        ease: "power1.in", 
+        y: -300 
+    });
+
+    tl.from(".subtitle",{ 
+        duration: 1, 
+        ease: "power1.in", 
+        y: -300  
+    });
+
+    tl.from(".button",{ 
+        duration: 1.5, 
+        ease: "bounce.inOut", 
+        y: -500
+    });
+
+
+};
+  
+  animate();
